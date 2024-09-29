21e39e4dcd0a56f666bfe9886b225ca2c790d8a3
Zice37 <dani243243@gmail.com>
2024-09-29 18:35:53 +0200

    suprimida salida de mysql, build de backend realizado con cache

--- launch_docker.sh
+++ launch_docker.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-sudo docker-compose stop
-sudo docker-compose rm -f backend
-sudo docker-compose build --no-cache backend
-sudo docker-compose up backend mysql
+# Detiene todos los servicios
+sudo docker-compose stop 
+
+# Elimina el contenedor del servicio backend
+sudo docker-compose rm -f backend 
+
+# Construye la imagen del servicio backend (usando la caché)
+sudo docker-compose build backend 
+
+# Inicia el servicio mysql en segundo plano
+sudo docker-compose up -d mysql 
+
+# Inicia el servicio backend y se adjunta a la terminal
+sudo docker-compose up backend
+
+sudo docker-compose stop 
--- restart_docker.sh
+++ restart_docker.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
-sudo docker-compose down --volumes
-sudo docker-compose build --no-cache
-sudo docker-compose up --build
+# Detiene y elimina los contenedores, redes y volúmenes definidos en el archivo docker-compose
+sudo docker-compose down --volumes 
+
+# Inicia el servicio mysql_db en segundo plano
+sudo docker-compose up --build -d mysql
+
+# Construye las imágenes sin usar la caché
+sudo docker-compose build --no-cache 
+
+# Inicia el servicio backend y se adjunta a la terminal
+sudo docker-compose up backend
+
+sudo docker-compose stop 
+
