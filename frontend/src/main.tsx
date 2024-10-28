37ac4b7ba8a9bc218eae1e67d0db484b57c6b674
Zice37 <dani243243@gmail.com>
2024-10-28 17:14:36 +0100

    paleta solucionada, pero da warnings al compilar

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-//import "bootstrap/dist/css/bootstrap.css";
-import "./scss/custom.scss";
+
+//PARA DESARROLLAR UTILIZAR PALETA POR DEFECTO Y CAMBIARLA SOLO PARA HACER LA BUILD A PRODUCCION
+import "bootstrap/dist/css/bootstrap.css";
+//import "./scss/custom.scss";
+//JUSTIFICAR LOS WARNING AL USO INTERNO DE @import EN BOOTSTRAP
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
