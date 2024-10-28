798876ca25208227e310c9def726c7232757deb2
Zice37 <dani243243@gmail.com>
2024-10-28 20:04:53 +0100

    paleta de colores resuelta

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-
-//PARA DESARROLLAR UTILIZAR PALETA POR DEFECTO Y CAMBIARLA SOLO PARA HACER LA BUILD A PRODUCCION
-import "bootstrap/dist/css/bootstrap.css";
-//import "./scss/custom.scss";
-//JUSTIFICAR LOS WARNING AL USO INTERNO DE @import EN BOOTSTRAP
+import "./css/custom.css"; //COMPILADO CON: sass --watch scss/custom.scss css/custom.css
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
