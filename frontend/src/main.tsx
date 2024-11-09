fc16ac5dc4da547f1f627f43d424ce75fd6d8baa
Zice37 <dani243243@gmail.com>
2024-11-09 12:52:17 +0100

    updated gitignore

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./css/customBootstrap/custom.css"; //COMPILADO CON: sass --watch scss/custom.scss css/customBootstrap/custom.css
+import "./css/customBootstrap/custom.css"; //COMPILADO CON: sass -q ./scss/custom.scss ./css/customBootstrap/custom.css
 import "./css/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
